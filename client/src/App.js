97455f8dc055481fe99f4bea44d0fd95be38a48c
Aaroneld <merrifield48@gmail.com>
2019-11-10 23:09:37 -0500

    MVP => Stretch 'Extra Custom Hook'

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Main from './components/Main';
+import Axios from 'axios';
+import { createGlobalStyle } from "styled-components";
 
-function App() {
-  return (
+const GlobalStyles = createGlobalStyle`
+
+  body {
+
+    background: white;
+    color: black;
+  }
+
+  .dark-mode {
+
+    background: black;
+    color: white;
+  }
+`;
+
+class  App extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      players: []
+
+    }  
+
+  }
+
+    
+  componentDidMount() {
+
+    const getPlayers = () => {
+      Axios.get("http://localhost:5000/api/players")
+      .then(response => {
+        let data = response.data;
+        this.setState({players: data});
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    }
+
+    getPlayers();
+    
+  }
+
+  render () { return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyles />
+      <Main players={this.state.players} />
     </div>
-  );
+   );
+  }
 }
 
 export default App;
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import Main from './components/Main';
+import PlayersList from './components/PlayersList'
+import { render, getByLabelText } from '@testing-library/react';
+import Axios from 'axios';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+
+
+test('renders without crashing', () => {
+  render(<App />);
 });
+
+test('contains title 1', () => {
+
+  const container = render(<App />)
+  const learn = container.getByText("Womens World Cup");
+})
+
+test('contains title 2', () => {
+
+  const container = render(<App />)
+  const learn = container.getByText("Players List");
+})
+
+
+
+
+
